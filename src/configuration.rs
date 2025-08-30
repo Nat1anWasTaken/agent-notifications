55feef8aaad095ec650abc3cbbb8aca8f4815cd0
Nat1anWasTaken <nathan@nat1an.xyz>
2025-08-30 13:37:50 +0800

    feat(*): implement the basic functions for claude code

---
+++ src/configuration.rs
@@ -0,0 +1,95 @@
+use std::{
+    collections::HashMap,
+    env, fs,
+    path::{Path, PathBuf},
+};
+
+use anyhow::Error;
+use serde::{Deserialize, Serialize};
+
+use crate::processors::claude::structs::HookEventName;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Claude {
+    pub allowed_hooks: HashMap<HookEventName, bool>,
+}
+
+impl Default for Claude {
+    fn default() -> Self {
+        let allowed_hooks = HashMap::from([
+            (HookEventName::Notification, true),
+            (HookEventName::PreToolUse, false),
+            (HookEventName::PostToolUse, false),
+            (HookEventName::UserPromptSubmit, false),
+            (HookEventName::Stop, true),
+            (HookEventName::SubagentStop, false),
+            (HookEventName::PreCompact, false),
+            (HookEventName::SessionStart, true),
+            (HookEventName::SessionEnd, true),
+        ]);
+
+        Claude { allowed_hooks }
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Config {
+    pub version: u32,
+    pub claude: Claude,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            version: 1,
+            claude: Claude::default(),
+        }
+    }
+}
+
+pub fn get_config_path() -> Option<PathBuf> {
+    let system_config_path = dirs::config_dir();
+
+    if let Some(mut path) = system_config_path {
+        path.push("agent_notifications/a-notifications.json");
+        return Some(path);
+    }
+
+    let mut current_dir = env::current_dir().ok()?;
+
+    current_dir.push("a-notifications.json");
+
+    Some(current_dir)
+}
+
+pub fn create_default_config(path: &Path) -> Result<(), Error> {
+    let default_config = Config::default();
+    let config_data = serde_json::to_string(&default_config)?;
+
+    if let Some(parent) = path.parent() {
+        fs::create_dir_all(parent)?;
+    }
+
+    std::fs::write(path, config_data)?;
+
+    Ok(())
+}
+
+pub fn initialize_configuration(
+    config_path: &Path,
+    reset_configuration: bool,
+) -> Result<Config, Error> {
+    if config_path.exists() && reset_configuration {
+        fs::remove_file(config_path).ok();
+    }
+
+    if !config_path.exists() {
+        create_default_config(&config_path)?;
+    }
+
+    let contents = fs::read_to_string(config_path)?;
+
+    let config: Config = serde_json::from_str(&contents)?;
+
+    Ok(config)
+}
